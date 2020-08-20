c0e378d842993ae639e2136c7893dad05ca80812
kiathwe97 <tank0276@e.ntu.edu.sg>
2020-08-20 09:05:58 +0800

    add welcome message

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,13 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
+        /*String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+                + "|____/ \\__,_|_|\\_\\___|\n"; */
+
+        String message = "Hello! I'm Duke\nWhat can I do for you?\nBye. Hope to see you again soon!";
+
+        System.out.println(message);
     }
 }
