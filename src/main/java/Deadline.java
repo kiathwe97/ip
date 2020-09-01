7fe3f8a2f960da854af629e0718ee1cb066d284c
kiathwe97 <tank0276@e.ntu.edu.sg>
2020-09-01 16:51:00 +0800

    Add Deadline class

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,2 +1,41 @@
-package PACKAGE_NAME;public class Deadline {
+import java.util.GregorianCalendar;
+public class Deadline extends Task{
+    //private GregorianCalendar dueDate;
+    private String dueDate;
+    public Deadline(String deadlineName, String dueDate){
+        super(deadlineName);
+        this.dueDate = dueDate;
+    }
+
+    public String toString(){
+        return "[D] " + super.toString()+ " (by: " + this.dueDate + ")";
+    }
+
+    public String getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(String dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    /*
+    public Deadline(String deadlineName, int year, int month, int date, int hour, int minute){
+        super(deadlineName);
+        GregorianCalendar dueDate = new GregorianCalendar(year, month, date, hour, minute);
+        this.dueDate = dueDate;
+
+    }
+
+    public GregorianCalendar getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(GregorianCalendar dueDate) { // can use this method to change the due date in future
+        this.dueDate = dueDate;
+    }
+
+    public String toString(){
+        return "[D] " + super.toString() + "(by: " + dueDate.toString() + ")"; //not sure of how this will turn out yet
+    }*/
 }
