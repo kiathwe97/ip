1c4c412b39ae9f51cf12c6336e32b3a09eae492a
kiathwe97 <tank0276@e.ntu.edu.sg>
2020-09-28 18:11:29 +0800

    A-JavaDoc

--- src/main/java/duke/task/ToDo.java
+++ src/main/java/duke/task/ToDo.java
@@ -1,14 +1,30 @@
 package duke.task;
 
+/**
+ * Corresponds to a Task that user has to do
+ */
 public class ToDo extends Task{
-
+    /**
+     * Constructor to create a ToDo object
+     * @param toDoName name of the Task to be done
+     * @throws DukeException if whitespace toDoName specified
+     */
     public ToDo(String toDoName) throws DukeException{
         super(toDoName);
     }
+
+    /**
+     * Returns details of ToDo in human readable form
+     * @return details of ToDo in human readable form
+     */
     public String toString(){
         return "[T] " + super.toString();
     }
 
+    /**
+     * Returns a string containing the details of the ToDo for storage
+     * @return string containing the details of the ToDo for storage
+     */
     public String toSaveFormat(){
         int isCompletedInt;
         if (this.isCompleted()){
